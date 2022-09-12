141d5acc60d2afb14bf2399e49c976cee226d3a0
AyeshaKhanzada <91547226+ayeshakhanzada@users.noreply.github.com>
2022-09-12 13:14:08 +0530

    Add files via upload

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAeEEodQHaqCkVLORP4LM0iOx773FhEmKM",
+    authDomain: "crypto-hunter-90a14.firebaseapp.com",
+    projectId: "crypto-hunter-90a14",
+    storageBucket: "crypto-hunter-90a14.appspot.com",
+    messagingSenderId: "954746497939",
+    appId: "1:954746497939:web:656e2647a8e4b53c709666"
+  };
+
+  export default firebaseConfig;
+  
